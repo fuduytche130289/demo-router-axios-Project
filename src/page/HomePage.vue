557c6c2c5eee98287d57ad3957635f869e8fc0ce
Cong Duy <rudbich199@gmail.com>
2022-04-08 10:37:53 +0700

    product-detail

--- src/page/HomePage.vue
+++ src/page/HomePage.vue
@@ -6,38 +6,35 @@
       <h4 class="text-2xl font-bold mt-5">Sản phẩm mới nhất</h4>
       <div class="grid  grid-cols-1 md:grid-rows-2 md:grid-cols-4 mt-5 gap-10">
 
-          <a href="" class="bg-white shadow col-span-1"
-             v-for="(cartsItem,cartsIndex) in carts" :key="cartsIndex">
-            <router-link :to="{name:'detail-product'}">
-            <div>
-              <img :src="cartsItem.image.full_image_path" class="z-10 w-full h-72 object-cover" alt="shoe">
-              <div class="space-y-4 mt-5 px-4">
-                <div class="flex justify-between">
-                  <h4 class="uppercase font-extrabold text-xl text-left">{{ cartsItem.name }}</h4>
-                  <button><i class="fa-regular fa-heart text-xl"></i></button>
-                </div>
-                <p class="font-bold text-2xl text-red-500 text-left"><span>{{ cartsItem.price }}</span></p>
+        <div class="bg-white shadow col-span-1"
+             v-for="(cartsItem,cartsIndex) in homeData" :key="cartsIndex">
+
+          <div>
+            <img :src="cartsItem.image.full_image_path" class="z-10 w-full h-72 object-cover" alt="shoe">
+            <div class="space-y-4 mt-5 px-4">
+              <div class="flex justify-between">
+                <h4 class="uppercase font-extrabold text-xl text-left">{{ cartsItem.name }}</h4>
+                <button><i class="fa-regular fa-heart text-xl"></i></button>
               </div>
-              <div class="flex w-full mt-5 items-center justify-between px-2">
-                <div class="flex space-x-2 font-bold">
-                  <span class="bi bi-plus cursor-pointer"><i class="fa-solid fa-plus"></i></span>
-                  <input type="text" readonly class="border rounded w-12 text-center font-medium" value="1">
-                  <span class="bi bi-dash cursor-pointer"><i class="fa-solid fa-minus"></i></span>
-                </div>
-                <div class="flex justify-between space-x-2">
-                  <button
-                      class="flex-grow-1 cursor-pointer block flex items-center justify-center bg-gray-800 p-3 text-white font-bold">
-                    Add to Cart
+              <p class="font-bold text-2xl text-red-500 text-left"><span>{{ cartsItem.price }}</span></p>
+            </div>
+            <div class="flex w-full mt-5 items-center justify-between pl-2">
+              <div class="flex space-x-2 font-bold">
+                <span class="bi bi-plus cursor-pointer"><i class="fa-solid fa-plus"></i></span>
+                <input type="text" readonly class="border rounded w-12 text-center font-medium" value="1">
+                <span class="bi bi-dash cursor-pointer"><i class="fa-solid fa-minus"></i></span>
+              </div>
+              <div class="gap-1 flex">
+                <router-link @click.native="scrollToTop" :to="{name:'detail-product',query:{id:cartsItem.id}} ">
+                  <button class="p-3 text-md bg-gray-900 text-white">
+                    Add To Cart
                   </button>
-                  <button
-                      class="flex-grow-1 px-4 cursor-pointer block flex items-center justify-center bg-gray-800 p-3 text-white font-bold">
-                    <i class="fa-solid fa-share-nodes text-xl"></i>
-                  </button>
-                </div>
+                </router-link>
+                <button class="p-3 text-md bg-gray-900 text-white"><i class="fa-solid fa-share-nodes"></i></button>
               </div>
             </div>
-            </router-link>
-          </a>
+          </div>
+        </div>
 
       </div>
     </div>
@@ -46,23 +43,31 @@
 </template>
 
 <script>
+import {homeService} from "../service/homeService";
+
 export default {
   name: "HomePage",
   data() {
     return {
-      carts: [],
+      homeData: {}
     }
   },
   methods: {
-    getData() {
-      fetch('http://128.199.211.17/api/v1/home')
-          .then((res) => res.json())
-          .then((data) => {
-            this.carts = data.lastProducts;
-            console.log(this.carts)
-          })
+    async getData() {
+      try {
+        const resp = await homeService.getHome();
+        this.homeData = resp.data.lastProducts;
+        console.log(this.homeData);
+
+      } catch (e) {
+        console.log(e);
+      }
     },
-  }, components: {},
+    scrollToTop() {
+      window.scrollTo(0,0);
+    }
+  },
+  components: {},
   mounted() {
     this.getData();
   }
--- src/page/ProductDetail.vue
+++ src/page/ProductDetail.vue
@@ -5,19 +5,19 @@
       <div class="grid lg:grid-cols-7 grid-cols-1 gap-4 space-x-5">
         <div class="grid grid-cols-2 col-span-3 gap-4 space-x-5">
           <div class="col-span-5">
-            <img class="w-full h-96 object-cover" :src="imgCart" alt="">
+            <img class="w-full h-96 object-cover" :src="productImg" alt="">
           </div>
-          <div class="flex " v-for="(imgCartProducts,indexCartProducts) in cartProducts.images"
-               :key="indexCartProducts">
-            <img class="w-20" :src="imgCartProducts.full_image_path" alt="Nike Jordan 3-image-6"
-                 @click="handleImgCart(imgCartProducts.full_image_path)"/>
+          <div class="flex " v-for="(imgProductDetailDataItem,productDetailDataIndex) in productDetailData.images"
+               :key="productDetailDataIndex">
+            <img class="w-20" :src="imgProductDetailDataItem.full_image_path" alt="Nike Jordan 3-image-6"
+                 @click="handleProductImg(imgProductDetailDataItem.full_image_path)"/>
           </div>
         </div>
 
         <div class="col-span-4 space-y-7">
-          <h1 class="text-3xl font-bold">Nike Jordan 3</h1>
+          <h1 class="text-3xl font-bold">{{productDetailData.name}}</h1>
           <p class="font-bold text-2xl text-red-500 text-left">
-            <span> 100</span>
+            <span>{{productDetailData.price}}</span>
           </p>
 
           <div class="flex space-x-2 items-center font-bold">
@@ -34,13 +34,13 @@
       </div>
       <div class="my-20">
         <h6 class="font-bold text-2xl">Mô tả sản phẩm</h6>
-        <p>{{ cartProducts.short_desc }}</p>
+        <p>{{ productDetailData.short_desc }}</p>
       </div>
       <div>
         <h6 class="font-bold text-2xl">Có thể bạn sẽ thích</h6>
         <div class=" grid lg:grid-cols-5 md:grid-cols-4 grid-cols-2 gap-4 mt-5">
           <div class="productItem w-auto h-auto shadow-lg shadow-black-500/50 relative"
-               v-for="(mightYouLikeProductsItem,indexMightYouLikeProducts) in mightYouLikeProducts"
+               v-for="(mightYouLikeProductsItem,indexMightYouLikeProducts) in mightYouLikeProductsData"
                :key="indexMightYouLikeProducts">
             <img class="w-full h-72 object-cover" :src="mightYouLikeProductsItem.image.full_image_path" alt="">
             <div class="mx-3 my-2 ">
@@ -59,30 +59,41 @@
 </template>
 
 <script>
+import {productDetailService} from "@/service/productDetailService";
+
 export default {
   data() {
     return {
-      cartProducts: [],
-      mightYouLikeProducts: [],
-      imgCart: '',
+      productDetailData: {},
+      mightYouLikeProductsData: {},
+      productImg:'',
+      productId:0,
     }
   },
   methods: {
-    getData() {
-      fetch('http://128.199.211.17/api/v1/products/4')
-          .then((response) => response.json())
-          .then((data) => {
-            this.cartProducts = data.product;
-            this.mightYouLikeProducts = data.mightYouLikeProducts;
-            this.imgCart = this.cartProducts.images[0].full_image_path
-          })
+    async getData() {
+      try {
+        const resp = await productDetailService.getProductDetail(this.productId);
+        this.productDetailData = resp.data.product;
+        this.mightYouLikeProductsData = resp.data.mightYouLikeProducts;
+        this.productImg = this.productDetailData.images[0].full_image_path;
+        console.log(this.productDetailData);
+        console.log(this.productImg);
+        setTimeout(()=>{
+          window.scroll(0,0)
+        },500)
+      } catch (e) {
+        console.log(e);
+      }
     },
-    handleImgCart(img) {
-      this.imgCart = img;
+    handleProductImg(img){
+      this.productImg = img;
     },
   },
   mounted() {
+    this.productId = this.$route.query.id;
     this.getData();
+
   },
   components: {}
 }
